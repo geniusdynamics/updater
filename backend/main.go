5b1d2785a613ff10b89fc09641d8eacf7cae00ec
Kemboi Elvis <kemboielvis22@gmail.com>
2026-01-25 04:32:26 +0300

    feat: few updates

--- backend/main.go
+++ backend/main.go
@@ -4,17 +4,22 @@ import (
 	"log"
 
 	"github.com/geniusdynamics/updater/backend/internal/config"
+	"github.com/geniusdynamics/updater/backend/internal/files"
 	"github.com/geniusdynamics/updater/backend/internal/git"
 )
 
 func main() {
 	cfg := config.NewConfig()
+	err := files.LoadEnv(".env")
+	if err != nil {
+		log.Println(err)
+	}
 
 	githubClient := git.NewGitHubClient(cfg)
 	if _, err := githubClient.GetRepositories(); err != nil {
 		log.Fatal(err)
 	}
-	_, err := githubClient.SearchRepositories("ns8-")
+	_, err = githubClient.SearchRepositories("ns8-")
 	if err != nil {
 	}
 }

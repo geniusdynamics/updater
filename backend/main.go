467b40285877ce588e7a9ff58a54d3e9d0a48c09
Kemboi Elvis <kemboielvis22@gmail.com>
2026-02-02 02:42:57 +0300

    feat: testing

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/geniusdynamics/updater/backend/internal/config"
@@ -9,17 +10,36 @@ import (
 )
 
 func main() {
-	cfg := config.NewConfig()
 	err := files.LoadEnv(".env")
 	if err != nil {
 		log.Println(err)
 	}
+	cfg := config.NewConfig()
 
 	githubClient := git.NewGitHubClient(cfg)
 	if _, err := githubClient.GetRepositories(); err != nil {
 		log.Fatal(err)
 	}
-	_, err = githubClient.SearchRepositories("ns8-")
+	repos, err := githubClient.SearchRepositories("ns8-")
 	if err != nil {
+	}
+	fileNames := map[string]bool{
+		"build-images.sh": true,
+	}
+	for i := range 4 {
+		repo := repos.Repositories[i]
+
+		dir, err := githubClient.CloneRepository(*repo.CloneURL)
+		if err != nil {
+			log.Fatalf("%s \n", err)
+		}
+		fmt.Printf("Github Repo: %s \n", dir)
+		images, err := files.FindDockerImages(dir, fileNames)
+		if err != nil {
+			log.Fatalf("An error occurred: %s \n", err)
+		}
+		for _, image := range images {
+			fmt.Printf("Image: %s, %s, %s \n", image.Registry, image.Repo, image.Tag)
+		}
 	}
 }
